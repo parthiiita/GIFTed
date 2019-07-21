9b57feb0af636fbca7e271c4d012c0e0214746b2
Parth Sharma <parth.iiita@gmail.com>
2019-07-21 15:57:43 +0530

    added formatting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 import "./App.css";
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import MTable from './MTable';
-class App extends Component{
+class App extends Component {
 
-  state={
+  state = {
     gifs: [],
     pausedGifs: {},
     fetchStatus: 'init'
   }
 
   componentDidMount() {
-    this.fetchGif();  
+    this.fetchGif();
   }
 
 
-  fetchGif =  (q) => {
+  fetchGif = (q) => {
     if (this.state.fetchStatus !== 'loading') {
-    this.setState({fetchStatus: 'loading'});
-     return fetch(`api/v1/gifs/search?api_key=c9fn3g2RczsgtPOnbNrxfRsaTvblCJ91&q=${q}&limit=10&offset=0&rating=G&lang=en`)
-    .then(re => Promise.resolve(re.json())).then((res) => this.setState({fetchStatus: 'success',gifs: [...this.state.gifs, ...res.data]}, () => console.log(this.state.gifs)));
+      this.setState({ fetchStatus: 'loading' });
+      return fetch(`api/v1/gifs/search?api_key=c9fn3g2RczsgtPOnbNrxfRsaTvblCJ91&q=${q}&limit=10&offset=0&rating=G&lang=en`)
+        .then(re => Promise.resolve(re.json())).then((res) => this.setState({ fetchStatus: 'success', gifs: [...this.state.gifs, ...res.data] }, () => console.log(this.state.gifs)));
     } else {
       return Promise.resolve();
     }
@@ -29,32 +29,23 @@ class App extends Component{
     console.log(e.target.value);
     this.fetchGif(e.target.value);
   };
-  // pausePlay = (gif) => {
-  //   const pausedGifs = Object.assign({}, this.state.pausedGifs);
-  //   if (pausedGifs[gif.id] === undefined) pausedGifs[gif.id] = false;
-  //   pausedGifs[gif.id] = !pausedGifs[gif.id];
-  //   this.setState({pausedGifs});
-  // }
+
 
   rowJSX = (data) => {
     return <div><a href={data.images.original.url}>{data.id}</a></div>
   }
 
-  
-  render(){
-    return(
-      <div className="App">
-      
-      <Router>
-        {/* <Route exact path="/" render={() => <div><h1> GIFTed !</h1>
-        <input type="text" onChange={this.onChangee}/>
-        <div style={{}}>{this.state.gifs.data.map(g => <img onClick={() => this.pausePlay(g)} style={{width:200, height: 200, margin:10, cursor: 'pointer'}} src={this.state.pausedGifs[g.id] ? g.images.original_still.url : g.images.original.url} />)}</div></div>} /> */}
-        <Route exact path="/"  render={() => <MTable 
-        headers={['ID', 'TYPE', 'TITLE', 'DATE', 'RATING', 'GIF']}
-        headerStyle={{width: '200px'}}
-        asyncStatus={this.state.fetchStatus}
-        totalItems={this.state.gifs.length} data={this.state.gifs} fetchData={this.fetchGif} rowJSX={this.rowJSX} onChangee={this.onChangee} />} />
 
+  render() {
+    return (
+      <div className="App">
+
+        <Router>
+          <Route exact path="/" render={() => <MTable
+            headers={['ID', 'TYPE', 'TITLE', 'DATE', 'RATING', 'GIF']}
+            headerStyle={{ width: '200px' }}
+            asyncStatus={this.state.fetchStatus}
+            totalItems={this.state.gifs.length} data={this.state.gifs} fetchData={this.fetchGif} rowJSX={this.rowJSX} onChangee={this.onChangee} />} />
         </Router>
       </div>
     );
--- src/MTable/MTable.jsx
+++ src/MTable/MTable.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from './MTable.css';
 import ReactDOM from 'react-dom';
 import $ from 'jQuery';
-//const totalItems = 100;
 const itemHeight = 100;
 const itemsRendered = 10;
 var scrollContainer = document.getElementById('scroll-container');
@@ -13,7 +12,7 @@ var items = $('.MRow');
 let lastFirstItem = 1;
 
 class MTable extends React.Component {
-    state ={
+    state = {
         paddingTop: 0,
         paddingBottom: 0,
         asyncStatus: 'init'
@@ -23,97 +22,73 @@ class MTable extends React.Component {
         scrollContainer = $('.scroll-container');
         itemContainer = $('.item-container');
         items = $('.MRow');
-        // scrollContainer.onscroll(() => {
-        //     this.setScrollState(scrollContainer.scrollTop());
-        // });
-        
-        //this.setScrollState({target:{scrollTop:0}});
-        //console.log(this.props.data)
     }
     componentWillReceiveProps(nextProps) {
-        //console.log('called', this.props.data,nextProps.data)
-        
-        if(!this.props.data.length && nextProps.data.length) {
-           // console.log('in')
-            this.setScrollState({target:{scrollTop:0}}, nextProps);
+
+        if (!this.props.data.length && nextProps.data.length) {
+            this.setScrollState({ target: { scrollTop: 0 } }, nextProps);
         }
     }
 
-     setScrollState = (scrollTop, props = this.props) => {
-         const totalItems = props.totalItems;
-        //  console.log({totalItems});
-        //  console.log(scrollTop.target.scrollTop, scrollContainer.scrollTop)
-        //const itemContainer = document.getElementById('item-container');
-        
+    setScrollState = (scrollTop, props = this.props) => {
+        const totalItems = props.totalItems;
         const itemsBefore = Math.min(Math.floor(scrollTop.target.scrollTop / itemHeight), totalItems - itemsRendered);
-      const itemsAfter = totalItems - (itemsBefore + itemsRendered);
-      //console.log({itemsBefore, itemsAfter});
-      //const items = $('.MRow')
-      items = $('.MRow');
-      const firstItem = itemsBefore + 1;
-      const lastItem = itemsBefore + itemsRendered;
-    console.log(items);
-    let start = itemsBefore + 1;
-    for (var i = 0 ; i < 10 ; i++) {
-        //console.log( i);
-        ReactDOM.render( props.rowJSX(props.data[start + i -1]), items[i]);
-        //items.eq(0).append(<div>{i+1}</div>)
-    }
-    //     if (firstItem > lastFirstItem) {
-            
-    //       itemContainer.append(items.eq(0).text('' + lastItem));
-    //   } else if (firstItem < lastFirstItem && firstItem > 0) {
-          
-    //       itemContainer.prepend(items.eq(itemsRendered - 1).text('' + firstItem));
-    //   }
-      
-      lastFirstItem = firstItem;
-      
-      const paddingTop = itemsBefore * itemHeight;
-      const paddingBottom = itemsAfter * itemHeight;
-      //console.log({paddingTop, paddingBottom});
-      this.setState({paddingTop, paddingBottom},  () => {
-          if (!this.state.paddingBottom) {
-             this.setState({asyncStatus: 'loading'})
-             props.fetchData().then(()=> this.setState({asyncStatus: 'success'}));
-           
-            
-          } 
+        const itemsAfter = totalItems - (itemsBefore + itemsRendered);
+        items = $('.MRow');
+        const firstItem = itemsBefore + 1;
+        const lastItem = itemsBefore + itemsRendered;
+        console.log(items);
+        let start = itemsBefore + 1;
+        for (var i = 0; i < 10; i++) {
+            ReactDOM.render(props.rowJSX(props.data[start + i - 1]), items[i]);
+        }
+
+
+        lastFirstItem = firstItem;
+
+        const paddingTop = itemsBefore * itemHeight;
+        const paddingBottom = itemsAfter * itemHeight;
+        this.setState({ paddingTop, paddingBottom }, () => {
+            if (!this.state.paddingBottom) {
+                this.setState({ asyncStatus: 'loading' })
+                props.fetchData().then(() => this.setState({ asyncStatus: 'success' }));
+
+
+            }
         });
-      //itemContainer.css({ paddingTop, paddingBottom });
-      //this.forceUpdate()
-;    }
+        ;
+    }
     render() {
-        console.log('render',this.state.asyncStatus, this.props.data)
+        console.log('render', this.state.asyncStatus, this.props.data)
         return (<div>
             <div><h1> GIFTed !</h1>
-        <input type="text" onChange={this.props.onChangee}/></div>
-        <table>
-            <thead>
-                <tr>
-                    {this.props.headers.map(h => <th style={this.props.headerStyle}>{h}</th>)}
+                <input type="text" onChange={this.props.onChangee} /></div>
+            <table>
+                <thead>
+                    <tr>
+                        {this.props.headers.map(h => <th style={this.props.headerStyle}>{h}</th>)}
                     </tr>
-            </thead>
-        </table>
+                </thead>
+            </table>
             <div onScroll={this.setScrollState} id="scroll-container" className="MContainer">
-        <div id="item-container" className="Table" style={{paddingTop: this.state.paddingTop, paddingBottom: this.state.paddingBottom}}>
-            
-            <div className="MRow">1</div>
-            <div className="MRow">2</div>
-            <div className="MRow">3</div>
-            <div className="MRow">4</div>
-            <div className="MRow">5</div>
-            <div className="MRow">6</div>
-            <div className="MRow">7</div>
-            <div className="MRow">8</div>
-            <div className="MRow">9</div>
-            <div className="MRow">10</div>
-            {this.props.asyncStatus === 'loading' && <div className="loader"></div>}
+                <div id="item-container" className="Table" style={{ paddingTop: this.state.paddingTop, paddingBottom: this.state.paddingBottom }}>
 
-        </div>
+                    <div className="MRow">1</div>
+                    <div className="MRow">2</div>
+                    <div className="MRow">3</div>
+                    <div className="MRow">4</div>
+                    <div className="MRow">5</div>
+                    <div className="MRow">6</div>
+                    <div className="MRow">7</div>
+                    <div className="MRow">8</div>
+                    <div className="MRow">9</div>
+                    <div className="MRow">10</div>
+                    {this.props.asyncStatus === 'loading' && <div className="loader"></div>}
 
-        </div>
-      
+                </div>
+
+            </div>
+
         </div>);
     }
 }
